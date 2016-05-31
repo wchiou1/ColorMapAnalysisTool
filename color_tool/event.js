d37f56f63e2d22d86200310842223ea548a3bc4e
ChenKuo <ckuo1@umbc.edu>
2016-05-31 16:11:00 -0400

    Add files via upload
    drag and drop file

---
+++ color_tool/event.js
@@ -0,0 +1,68 @@
+
+var image2DArray=[];
+var imageWidth;
+var imageHeight;
+$(document).on('load',dropInit());
+function dropInit(){
+if(window.FileReader) { 
+  addEventHandler(window, 'load', function() {
+    var status = document.getElementById('status');
+    var drop   = document.getElementById('drop');
+    var list   = document.getElementById('list');
+  	
+    function cancel(e) {
+       e.preventDefault(); 
+	  console.log('something');
+    }
+  
+    // Tells the browser that we *can* drop on this target
+	
+    addEventHandler(drop, 'dragover', cancel);
+    addEventHandler(drop, 'dragenter', cancel);
+	addEventHandler(drop,'drop', function(e) {
+		e = e || window.event;
+		cancel(e);
+        e.stopPropagation();
+        
+        var files = e.dataTransfer.files; // Array of all files
+        for (var i=0, file; file=files[i]; i++) {
+                var reader = new FileReader();
+                reader.onload = function(e2) { // finished reading file data.
+					//parse the data into the array
+                    var lines=e2.target.result.split('\n');
+					imageHeight=lines.length;
+					for(var i=0;i<lines.length;i++) {
+						var values=lines[i].split(' ');
+						if(!imageWidth){
+							imageWidth = values.length;
+						}else if(imageWidth!=values.length){
+							//alert('error reading the file. line'+i+ ",num"+values.length);
+						}
+						for(var j=0; j<values.length; j++){
+							image2DArray.push(values[j]);
+						}
+					}
+					console.log(image2DArray);
+                }
+                reader.readAsText(file); // start reading the file data.
+			}
+		}) 
+  });
+} else { 
+  document.getElementById('status').innerHTML = 'Your browser does not support the HTML5 FileReader.';
+}
+}
+
+
+function addEventHandler(obj, evt, handler) {
+    if(obj.addEventListener) {
+        // W3C method
+        obj.addEventListener(evt, handler, false);
+    } else if(obj.attachEvent) {
+        // IE method.
+        obj.attachEvent('on'+evt, handler);
+    } else {
+        // Old school method.
+        obj['on'+evt] = handler;
+    }
+}
